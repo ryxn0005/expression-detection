40c89a06883f42deb668e7f637be1f62ffaca50b
ryxn0005 <billnguyen1010@gmail.com>
2025-10-09 18:14:55 +1100

    minor changes

--- src/utilities/preprocessing/data_loader.py
+++ src/utilities/preprocessing/data_loader.py
@@ -109,8 +109,6 @@ def load_data(
     X = np.array([d[0] for d in data], dtype=np.float32)
     y = np.array([d[1] for d in data], dtype=np.int64)
 
-    X = (X / 255)
-
     # ensure channel dim for Keras
     if X.ndim == 3:
         X = X[..., None]  # (N, H, W, 1)
